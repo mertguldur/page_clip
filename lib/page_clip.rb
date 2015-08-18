d8bead9f6a9cab20c22470358b7126d10828cd2a
Mert Guldur <mguldur@enova.com>
2015-08-18 17:01:25 -0500

    Refactor core functionality

--- lib/page_clip.rb
+++ lib/page_clip.rb
@@ -1,26 +1,24 @@
 class PageClip
-  def initialize(total_item_count, current_page, per_page, options = {})
-    @total_item_count       = total_item_count
-    @current_page           = current_page
-    @per_page               = per_page
-    @pages_to_display_count = options[:pages_to_display_count] || 5
-
-    @total_page_count = total_page_count
-    @total_pages      = total_pages
-    @side_page_count  = side_page_count
+  def initialize(item_count, per_page, current_page, pages_to_show_count)
+    @current_page = current_page
+    @page_count = (item_count.to_f / per_page.to_f).ceil
+    @pages = (1..@page_count).to_set
+    @side_pages_to_show_count = (pages_to_show_count - 1) / 2
   end
 
-  def self.calculate(total_item_count, current_page, per_page, options = {})
-    new(total_item_count, current_page, per_page, options).calculate
+  def self.show(item_count:, per_page:, current_page:, pages_to_show_count:)
+    new(item_count, per_page, current_page, pages_to_show_count).calculate
   end
 
   def calculate
-    [pages_to_display, @total_page_count]
+    [pages_to_show, @page_count]
   end
 
   private
 
-  def pages_to_display
+  def pages_to_show
+    return [] if @page_count.zero?
+
     pages = [@current_page]
 
     pages_to_add, left_missed_page_count = side_pages_to_add(:left)
@@ -44,26 +42,14 @@ class PageClip
   def side_pages_to_add(direction, extra_page_count = 0)
     pages = []
     page = @current_page
-    pages_to_add_count = @side_page_count + extra_page_count
+    pages_to_add_count = @side_pages_to_show_count + extra_page_count
 
     pages_to_add_count.times do
       page = direction == :left ? page - 1 : page + 1
-      pages << page if @total_pages.include?(page)
+      pages << page if @pages.include?(page)
     end
 
     missed_page_count = pages_to_add_count - pages.size
     [pages, missed_page_count]
-  end
-
-  def total_page_count
-    (@total_item_count.to_f / @per_page.to_f).ceil
-  end
-
-  def total_pages
-    (1..@total_page_count).to_set
-  end
-
-  def side_page_count
-    (@pages_to_display_count - 1) / 2
   end
 end
